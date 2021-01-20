c6d2192fdfe0bd2cd16700a2812b0fb4f3ae1ded
Tony <mahaitao_pony@hotmail.com@users.norepy.github.com>
2021-01-20 22:06:31 +1100

    credit card list

--- Wallet/Wallet/CardListView.swift
+++ Wallet/Wallet/CardListView.swift
@@ -30,19 +30,11 @@ struct CardList: View {
                             .opacity((Double(geometry.frame(in: .global).minY)+30)/25)
                             .offset(y: geometry.frame(in: .global).minY > 0 ? 0: -geometry.frame(in: .global).minY )
                             .animation(.none)
-                        
                     }.frame(height:88)
-//                    Text("Wallet")
-//                        .font(.largeTitle).bold()
-//                        .frame(maxWidth: .infinity, alignment: .leading)
-//                        .padding(.leading, 30)
-//                        .padding(.top, 30)
-//                        .blur(radius: active ? 20 : 0)
-//                      .background(Color.black.opacity(0.5))
                     
                     ForEach(cardsArray.indices, id: \.self) { index in
                         GeometryReader { geometry in
-                            CreditCardView(
+                            CreditCardDetailView(
                                 show: self.$cards[index].show,
                                 card: self.cards[index],
                                 active: self.$active,
@@ -55,7 +47,7 @@ struct CardList: View {
                                 .scaleEffect(self.activeIndex != index && self.active ? 0.5 : 1)
                                 .offset(x: self.activeIndex != index && self.active ? screen.width : 0)
                         }
-                        .frame(height: 180)
+                        .frame(height: 210)
                         .frame(maxWidth: self.cards[index].show ? .infinity : screen.width - 60)
                         .zIndex(self.cards[index].show ? 1 : 0)
                     }
@@ -75,7 +67,7 @@ struct CardList_Previews: PreviewProvider {
     }
 }
 
-struct CreditCardView: View {
+struct CreditCardDetailView: View {
     @Binding var show: Bool
     var card: CreditCard
     @Binding var active: Bool
@@ -85,41 +77,17 @@ struct CreditCardView: View {
     
     var body: some View {
         ZStack(alignment: .top) {
-            VStack(alignment: .leading, spacing: 10.0) {
-                Text("Latest Transactions").font(.system(size: 28, weight: .bold, design: .rounded)).frame(maxWidth: .infinity, alignment: .leading)
-                ForEach(transactions){t in
-                    HStack {
-                        VStack(alignment: .leading){
-
-                            Text("\(t.title)").font(.system(size: 16, weight: .bold, design: .rounded))
-                            Text("\(t.location)").font(.subheadline)
-                            Text("\(t.date)").font(.caption)
-                        }
-                        Spacer()
-                        Text("\(t.amount)")
-                    }
-                    Divider()
-                }
-            }
-//            .background(Color.black.opacity(0.3))
-            .padding(.horizontal, 30)
-            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
-            .offset(y: show ? 320 : 0)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .opacity(show ? 1 : 0)
+            TransactionListView(show: $show)
             
             VStack {
                 Image("\(card.image)").resizable().aspectRatio(contentMode: .fit )
             }
             .padding(show ? 30 : 20)
-//            .padding(.top, 0)
             .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
             .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 300 : 220)
-//            .background(card.color)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: card.color.opacity(0.3), radius: 20, x: 0, y: 20)
+            .shadow(color: card.color.opacity(0.1), radius: 1, x: 0, y: 1)
             .gesture(
                 show ?
                 DragGesture().onChanged { value in
@@ -180,3 +148,32 @@ struct CreditCardView: View {
 let screen = UIScreen.main.bounds
 let herizentalMarginWidth: CGFloat = 30
 let statusBarHeight: CGFloat = 44
+
+struct TransactionListView: View {
+    @Binding var show:Bool
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10.0) {
+            Text("Latest Transactions").font(.system(size: 28, weight: .bold, design: .rounded)).frame(maxWidth: .infinity, alignment: .leading)
+            ForEach(transactions){t in
+                HStack {
+                    VStack(alignment: .leading){
+                        
+                        Text("\(t.title)").font(.system(size: 16, weight: .bold, design: .rounded))
+                        Text("\(t.location)").font(.subheadline)
+                        Text("\(t.date)").font(.caption)
+                    }
+                    Spacer()
+                    Text("\(t.amount)")
+                }
+                Divider()
+            }
+        }
+        .padding(.horizontal, 30)
+        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
+        .offset(y: show ? 320 : 0)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+        .opacity(show ? 1 : 0)
+    }
+}
